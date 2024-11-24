80ef990f572db00bd23b8cd67596ea47a55f3b3d
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-24 19:40:49 +0330

    Calling classes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overLay = document.querySelector('.overlay');
+const btnColseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
