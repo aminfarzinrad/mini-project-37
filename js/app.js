ca5a856c4a82f734a538425aa517a00d26a528f8
AminFarzinrad <a.farzinrad062@gmail.com>
2024-11-24 19:44:38 +0330

    open modal

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ const modal = document.querySelector('.modal');
 const overLay = document.querySelector('.overlay');
 const btnColseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for(let i = 0; i < btnsOpenModal.length; i++){
+    document.addEventListener('click',
+        function(){
+            modal.classList.remove('hidden');
+            overLay.classList.remove('hidden');
+        }
+    )
+};
